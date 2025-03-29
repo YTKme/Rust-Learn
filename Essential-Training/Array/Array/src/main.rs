318f10b6cc03c028dd3e2f846f82139fa591c1b2
Yan Kuang <YTKme@Outlook.com>
2025-03-29 15:22:39 -0700

    Add Array lesson code

---
+++ Essential-Training/Array/Array/src/main.rs
@@ -0,0 +1,39 @@
+fn main() {
+    println!("Array");
+
+    let letter_array = ['a', 'b', 'c'];
+    println!("Letter Array: {:?}", letter_array);
+
+    let first_letter = letter_array[0];
+    println!("The first letter is: {first_letter}");
+
+    // Mutable Array
+    let mut number_array = [1, 2, 3, 4, 5];
+    println!("Number Array: {:?}", number_array);
+
+    number_array[1] = 10;
+    let second_number = number_array[1];
+    println!("The second number is: {second_number}");
+
+    // Uninitialized Array
+    let empty_number_array: [i32; 5];
+    // empty_number_array = [0, 0, 0, 0, 0];
+    empty_number_array = [0; 5]; // Repeat expression
+    let index: usize = empty_number_array.len() - 1;
+    println!("Empty Number Array Index: {:?}", index);
+
+    // Multidimensional Array (2D Array)
+    let parking_lot = [
+        [1, 2, 3], // Row 0
+        [4, 5, 6], // Row 1
+        [7, 8, 9], // Row 2
+    ];
+    let parking_spot = parking_lot[1][2];
+    println!("Parking Lot: {:?}", parking_lot);
+    println!("Parking Spot: {parking_spot}");
+
+    // Multidimensional Array (3D Array)
+    // let garage = [[[0; 100]; 20]; 5];
+    let garage = [[[0; 10]; 3]; 5];
+    println!("Garage: {:?}", garage);
+}
