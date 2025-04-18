385ab00fe59edabadc4c3d18acb7ff890d63d9d1
Yan Kuang <YTKme@Outlook.com>
2025-04-18 14:30:12 -0700

    Refactor Compound-Data-Types code

--- Essential-Training/Compound-Data-Types/src/main.rs
+++ Essential-Training/Compound-Data-Types/src/main.rs
@@ -36,4 +36,22 @@ fn main() {
     // let garage = [[[0; 100]; 20]; 5];
     let garage = [[[0; 10]; 3]; 5];
     println!("Garage: {:?}", garage);
+
+    println!("Tuple");
+
+    // Tuple Basic
+    let stuff_tuple: (u8, f32, char) = (10, 3.14, 'x');
+    println!("Stuff Tuple: {:?}", stuff_tuple);
+    let first_stuff = stuff_tuple.0;
+    println!("First Stuff: {first_stuff}");
+
+    // Mutable Tuple
+    let mut mutable_tuple: (u8, f32, char) = (10, 3.14, 'x');
+    println!("Mutable Tuple: {:?}", mutable_tuple);
+    mutable_tuple.0 += 20;
+    println!("Mutable Tuple: {:?}", mutable_tuple);
+
+    // Unpack Tuple
+    let (first_stuff, second_stuff, third_stuff) = stuff_tuple;
+    println!("Unpacked Stuff Tuple: First: {first_stuff}, Second: {second_stuff}, Third: {third_stuff}");
 }
