34403a378dc4f4ce75eb335170a4c746b8d51a3a
Yan Kuang <YTKme@Outlook.com>
2025-04-26 11:27:42 -0700

    Update Modules lesson code

--- Essential-Training/Modules/src/main.rs
+++ Essential-Training/Modules/src/main.rs
@@ -1,5 +1,29 @@
+use std::io;
+use rand::{self, thread_rng};
+use rand::prelude::*;
+
 fn main() {
     println!("Module");
 
     println!("Standard Library");
+
+    println!("Standard Input");
+
+    // let mut buffer = String::new();
+    // println!("Enter a Message: ");
+    // let _ = io::stdin().read_line(&mut buffer);
+    // println!("You Entered: {buffer}");
+
+    println!("Parse String");
+
+    // let number: i32 = buffer.trim().parse::<i32>().unwrap();
+    // println!("Your Number + 1: {}", number + 1);
+
+    println!("Crate");
+
+    let random_number = rand::random::<f64>();
+    println!("Random Number: {random_number}");
+
+    let number = thread_rng().gen_range(1..11);
+    println!("Thread RNG: {:?}", number);
 }
